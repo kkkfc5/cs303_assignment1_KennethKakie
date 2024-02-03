d6fea70318f5d4156ce88f056e573ecf6cf07aee
kkkfc5 <143641376+kkkfc5@users.noreply.github.com>
2024-02-03 16:14:48 -0600

    Add files via upload

--- cs303_assignment1_KennethKakie/cs303_assignment1_KennethKakie/functions.h
+++ cs303_assignment1_KennethKakie/cs303_assignment1_KennethKakie/functions.h
@@ -6,12 +6,12 @@
 
 using namespace std;
 
+// - - - - - General - - - - - 
 void Print(int userArray[], int userArraySize);
-
-
-
 void ReadFile();
 
+
+// - - - - - Assignment - - - - - 
 
 //1) A function to check if a certain integer exists in the array if the number is present return the index where the number is present.
 int CheckForInteger(int userArray[], int userArraySize);
@@ -27,20 +27,3 @@ void AddToEnd(int userArray[], int &userArraySize);
 
 //4) A function which intakes an index of an array and replaces the value with either 0 or removes the integer altogether.
 void DeletingValue(int userArray[], int userArraySize);
-
-
-
-//Q2)
-//Add a try and catch blocks to the user inputs for the following functions from question 1:
-   //A function that can modify the value of an integer when called with the index of the integer in the array and return the new value and old value back to the user.
-   //A function that adds a new integer to the end of the array
-
-
-
-
-
-
-
-// 1 IS STANDALONE ,
-
-// 2 FLOWS INTO 3, AND THAT FLOWS INTO 4
